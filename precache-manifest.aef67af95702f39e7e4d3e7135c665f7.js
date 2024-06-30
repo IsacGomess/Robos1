83799e7e0031459765224974ce2879a1a6cf7fdb
Isac Gomes <isacfisio@gmail.com>
2024-06-30 06:53:46 -0300

    Updates

---
+++ precache-manifest.aef67af95702f39e7e4d3e7135c665f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b5d43266e8a544bba6cc9cda662e4f",
+    "url": "/Robos1/index.html"
+  },
+  {
+    "revision": "ebd1f7ec0526befc4586",
+    "url": "/Robos1/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "39aefbfabf972eb40495",
+    "url": "/Robos1/static/css/main.ad38077c.chunk.css"
+  },
+  {
+    "revision": "ebd1f7ec0526befc4586",
+    "url": "/Robos1/static/js/2.b5881a2f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Robos1/static/js/2.b5881a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ecc8d4759925795b22",
+    "url": "/Robos1/static/js/3.7f0ccf67.chunk.js"
+  },
+  {
+    "revision": "39aefbfabf972eb40495",
+    "url": "/Robos1/static/js/main.c576110e.chunk.js"
+  },
+  {
+    "revision": "3390e1bac639d162a108",
+    "url": "/Robos1/static/js/runtime-main.a83749ff.js"
+  }
+]);
